80613a00137ce57be3de7234d5ff06e3c277d6a0
Brian Norman <brian.e.norman@gmail.com>
2014-04-16 21:45:08 -0500

    Initial commit - added initial project files
    Welcome to the JavaGL project.  This project's aim is to wrap the LWJGL
    library with an object oriented approach to working with OpenGL.

---
+++ src/main/java/com/game30/graphics/javagl/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the classes needed to work with OpenGL.  Since LWJGL is the core library being used to work with OpenGL,
+ * all the classes are wrappers of the basic functionality.  Usually, the class is an object representation of the
+ * objects in OpenGL instead of a group of static calls into OpenGL.  This creates a better object oriented design to
+ * work with OpenGL.
+ *
+ * @author Brian Norman
+ * @since 1.0
+ */
+package com.game30.graphics.javagl;
