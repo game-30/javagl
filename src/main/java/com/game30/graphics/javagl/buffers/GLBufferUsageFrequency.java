e2cc889444f9aaf1bc1ae371495a210753908a0d
Brian Norman <brian.e.norman@gmail.com>
2014-04-20 12:14:41 -0500

    Added enumeration that describes OpenGL buffer usage frequency
    OpenGL buffers come in a lot of different flavors.  Part of that flavor
    is how frequently a buffer is written.  This enumeration describes how
    frequently an OpenGL buffer can be written.

---
+++ src/main/java/com/game30/graphics/javagl/buffers/GLBufferUsageFrequency.java
@@ -0,0 +1,33 @@
+package com.game30.graphics.javagl.buffers;
+
+/**
+ * An enumeration of how frequently a OpenGL buffer can be written.  Frequency is very ambiguous and should be treated
+ * as such.  Performance on different hardware and software should be evaluated before choosing a specific frequency.
+ *
+ * @author Brian Norman
+ * @since 1.0
+ */
+public enum GLBufferUsageFrequency {
+
+    /**
+     * A static buffer is a buffer that is usually written to once and never changed.  This is only a guideline though,
+     * because there is no restriction to changing a static buffer.
+     */
+    Static,
+
+    /**
+     * A dynamic buffer is a buffer that is written to often.  This is the most ambiguous of all frequencies because it
+     * is the middle ground between static and stream.  Usually if a buffer does not fall into either the static or
+     * stream category, it should be a dynamic buffer.
+     */
+    Dynamic,
+
+    /**
+     * A stream buffer is a buffer that is usually written to just as much as it is read from.  This is only a guideline
+     * though, because there is no restriction to never writing to a stream again after the initial write.
+     */
+    Stream,
+
+    // End of enumeration
+    ;
+}
