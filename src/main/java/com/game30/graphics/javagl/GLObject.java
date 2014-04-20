507edf6924011010a1911b7eba30c9d1291aeaed
Brian Norman <brian.e.norman@gmail.com>
2014-04-20 11:52:50 -0500

    Added base interface for conventional OpenGL objects
    Most OpenGL objects have the same functionality.  This is a combination
    of that basic functionality into a common interface.  This interface
    specifies that an OpenGL object is indexed, can be bound, and can be
    deleted.

---
+++ src/main/java/com/game30/graphics/javagl/GLObject.java
@@ -0,0 +1,21 @@
+package com.game30.graphics.javagl;
+
+/**
+ * An OpenGL construct that contains some state.  They can be bound to the current context and their state is added to
+ * the context.  Changes to the current context will effect any bound object's state.  Since OpenGL is a state machine,
+ * this is how changes to objects are made.
+ *
+ * @author Brian Norman
+ * @see <a href="http://www.opengl.org/wiki/OpenGL_Object">OpenGL Object on OpenGL wiki</a>
+ * @since 1.0
+ */
+public interface GLObject extends GLIndexed, GLDeletable, GLBindable {
+    /* Implementation Details:
+     *     This interface should contain functionality that is core to all OpenGL objects.  Anything that is considered
+     *     an object in OpenGL should inherit from this interface.  However, some things have the same functionality as
+     *     an OpenGL object but are not considered true OpenGL objects.  This is why certain functionality is broken out
+     *     into separate interfaces.
+     */
+
+    // Combined implementation of GLIndexed, GLDeletable, and GLBindable
+}
