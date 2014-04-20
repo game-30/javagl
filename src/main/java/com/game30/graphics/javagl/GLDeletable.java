a1d16d0e78c005a00cb2514ce557bf138b821cbd
Brian Norman <brian.e.norman@gmail.com>
2014-04-20 11:47:03 -0500

    Added interface for OpenGL objects that can be deleted
    Any OpenGL object that can be created, needs to be deleted.  This
    interface describes those objects and also allows for querying of the
    object's current state - if it exists or not.

---
+++ src/main/java/com/game30/graphics/javagl/GLDeletable.java
@@ -0,0 +1,42 @@
+package com.game30.graphics.javagl;
+
+/**
+ * Describes an object in OpenGL that can be deleted.
+ *
+ * @author Brian Norman
+ * @since 1.0
+ */
+public interface GLDeletable {
+
+    /**
+     * Deletes the OpenGL object.  The object does not need to be bound to delete the object but other objects
+     * associated with the object will need to be deleted in sequence.
+     */
+    void delete();
+
+    /**
+     * Returns {@code true} if the OpenGL object exists.  This may or may not make the request of OpenGL or just keep
+     * the state internally.
+     *
+     * @return if the OpenGL object exists.
+     */
+    boolean exists();
+
+
+    // ============================== //
+    // ***** Static GLDeletable ***** //
+    // ============================== //
+
+    /**
+     * Validates that the GLDeletable still exists.  If it does not exist, a NullPointerException is thrown.
+     *
+     * @param deletable the OpenGL deletable object that needs to exist.
+     * @throws NullPointerException if the OpenGL deletable object does not exist.
+     */
+    static <T extends GLDeletable> T requireExists(T deletable) {
+        if (!deletable.exists()) {
+            throw new NullPointerException("OpenGL deletable [" + deletable + "] does not exist!");
+        }
+        return deletable;
+    }
+}
