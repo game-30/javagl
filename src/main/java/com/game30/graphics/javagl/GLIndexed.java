b672058e267b9a11c06b108e2e177990195ac66e
Brian Norman <brian.e.norman@gmail.com>
2014-04-19 19:30:18 -0500

    Added interface for OpenGL objects that are referenced by index
    Keepin' it simple with the first few check-ins.  This interface
    specifies the index by which an OpenGL object can be referenced.  This
    is common in a lot of OpenGL objects like buffers, textures, programs,
    and shaders.

---
+++ src/main/java/com/game30/graphics/javagl/GLIndexed.java
@@ -0,0 +1,28 @@
+package com.game30.graphics.javagl;
+
+/**
+ * Describes an object in OpenGL that is referenced by index.
+ *
+ * @author Brian Norman
+ * @since 1.0
+ */
+public interface GLIndexed {
+
+    /**
+     * Returns the index, also known as the GLuint, of the OpenGL object.  This is the integer returned by the object
+     * generation call to OpenGL.
+     *
+     * @return the index.
+     */
+    int getIndex();
+
+
+    // ============================ //
+    // ***** Static GLIndexed ***** //
+    // ============================ //
+
+    /**
+     * Object null index.  This is essentially the NULL pointer for OpenGL.
+     */
+    int NULL_INDEX = 0;
+}
