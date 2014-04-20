37505d5654058e5ef1bae0d45dc07a2133f9ccfe
Brian Norman <brian.e.norman@gmail.com>
2014-04-20 11:55:52 -0500

    Created interface for all the OpenGL 'magic integers'
    OpenGL has a lot of global integers that represent something specific.
    For example, OpenGL buffers can be of many different types but they are
    all represented by global integers.  This is not very type safe and this
    interface is going to help fix that.

---
+++ src/main/java/com/game30/graphics/javagl/GLMagicInteger.java
@@ -0,0 +1,19 @@
+package com.game30.graphics.javagl;
+
+/**
+ * A simple interface represents a OpenGL magic integer.  Magic integers are the integer values that represent something
+ * unique.  Usually these are encapsulated within a static integer but the binding is still weak.  This interface allows
+ * an enumeration to define the valid options and still supply the lower level integer to OpenGL.
+ *
+ * @author Brian Norman
+ * @since 1.0
+ */
+public interface GLMagicInteger {
+
+    /**
+     * Returns the magic integer that OpenGL understands.
+     *
+     * @return the magic integer.
+     */
+    int glInt();
+}
