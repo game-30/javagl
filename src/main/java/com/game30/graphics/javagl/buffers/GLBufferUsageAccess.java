c712eb1136b7f8099b219b6d5f71359905aa96c6
Brian Norman <brian.e.norman@gmail.com>
2014-04-20 12:11:42 -0500

    Added enumeration that describes OpenGL buffer usage access
    OpenGL buffers come in a lot of different flavors.  Part of that flavor
    is how the buffer is accessed.  This enumeration describes the different
    ways an OpenGL buffer can be accessed.

---
+++ src/main/java/com/game30/graphics/javagl/buffers/GLBufferUsageAccess.java
@@ -0,0 +1,32 @@
+package com.game30.graphics.javagl.buffers;
+
+/**
+ * An enumeration of the different ways a program can interact with an OpenGL buffer.
+ *
+ * @author Brian Norman
+ * @since 1.0
+ */
+public enum GLBufferUsageAccess {
+
+    /**
+     * A write access buffer is a buffer that the program writes to and OpenGL reads from.  The program cannot read from
+     * the buffer nor can OpenGL write to it.  This is used to give information to OpenGL for operations, such as
+     * drawing.
+     */
+    Write,
+
+    /**
+     * A read access buffer is a buffer that the program read from and OpenGL write to.  The program cannot write to the
+     * buffer nor can OpenGL read from it.  This is used to get information from OpenGL, such as transformations.
+     */
+    Read,
+
+    /**
+     * A none access buffer is a buffer that the program cannot read from or write to but OpenGL is able to both read
+     * from and write to.   This is used to transfer information within OpenGL between operations.
+     */
+    None,
+
+    // End of enumeration
+    ;
+}
