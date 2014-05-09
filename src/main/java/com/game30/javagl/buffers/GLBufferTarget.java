6ea58f0bc16aceaada6d87a537223c849aee088e
Brian Norman <brian.e.norman@gmail.com>
2014-05-08 21:01:15 -0500

    Reordered the buffer targets by OpenGL version
    The OpenGL buffer targets where previously ordered by how they are
    listed in the documentation.  The have been reordered by OpenGL version
    but also keeping the same general order from the documentation.

--- src/main/java/com/game30/javagl/buffers/GLBufferTarget.java
+++ src/main/java/com/game30/javagl/buffers/GLBufferTarget.java
@@ -38,6 +38,24 @@ public enum GLBufferTarget implements GLMagicInteger {
     ElementArray(GL15.GL_ELEMENT_ARRAY_BUFFER, GL15.GL_ELEMENT_ARRAY_BUFFER_BINDING),
 
     /**
+     * This is for performing asynchronous pixel transfer operations.  If a buffer is bound to PixelPack, glGetTexImage​
+     * and glReadPixels​ will store their data to the bound buffer object instead of where a client pointer points.
+     */
+    PixelPack(GL21.GL_PIXEL_PACK_BUFFER, GL21.GL_PIXEL_PACK_BUFFER_BINDING),
+
+    /**
+     * This is for performing asynchronous pixel transfer operations.  If a buffer is bound to PixelUnpack,
+     * glTexImage*​, glTexSubImage*​, glCompressedTexImage*​, and glCompressedTexSubImage*​ are all affected.  These
+     * functions will read their data from the bound buffer object instead of where a client pointer points.
+     */
+    PixelUnpack(GL21.GL_PIXEL_UNPACK_BUFFER, GL21.GL_PIXEL_UNPACK_BUFFER_BINDING),
+
+    /**
+     * An indexed buffer binding for buffers used in Transform Feedback operations.
+     */
+    TransformFeedback(GL30.GL_TRANSFORM_FEEDBACK_BUFFER, GL30.GL_TRANSFORM_FEEDBACK_BUFFER_BINDING),
+
+    /**
      * This has no particular semantics.  Because it has no actual meaning, it is a useful target for copying buffer
      * object data with glCopyBufferSubData​.  You do not have to use this target when copying, but by using it, you
      * avoid disturbing buffer targets that have actual semantics.
@@ -52,35 +70,10 @@ public enum GLBufferTarget implements GLMagicInteger {
     CopyWrite(GL31.GL_COPY_WRITE_BUFFER, GL31.GL_COPY_WRITE_BUFFER_BINDING),
 
     /**
-     * This is for performing asynchronous pixel transfer operations.  If a buffer is bound to PixelPack, glGetTexImage​
-     * and glReadPixels​ will store their data to the bound buffer object instead of where a client pointer points.
-     */
-    PixelPack(GL21.GL_PIXEL_PACK_BUFFER, GL21.GL_PIXEL_PACK_BUFFER_BINDING),
-
-    /**
-     * This is for performing asynchronous pixel transfer operations.  If a buffer is bound to PixelUnpack,
-     * glTexImage*​, glTexSubImage*​, glCompressedTexImage*​, and glCompressedTexSubImage*​ are all affected.  These
-     * functions will read their data from the bound buffer object instead of where a client pointer points.
-     */
-    PixelUnpack(GL21.GL_PIXEL_UNPACK_BUFFER, GL21.GL_PIXEL_UNPACK_BUFFER_BINDING),
-
-    /**
-     * This is for performing direct writes from asynchronous queries to buffer object memory.  If a buffer is bound to
-     * Query, then all glGetQueryObject​ function calls will write the result to an offset into the bound buffer
-     * object.
-     */
-    Query(GL44.GL_QUERY_BUFFER, GL44.GL_QUERY_BUFFER_BINDING),
-
-    /**
      * This target has no special semantics, but if you intend to use a buffer object for Buffer Textures, it is a good
      * idea to bind it here when you first create it.
      */
     Texture(GL31.GL_TEXTURE_BUFFER, GL31.GL_TEXTURE_BINDING_BUFFER),
-
-    /**
-     * An indexed buffer binding for buffers used in Transform Feedback operations.
-     */
-    TransformFeedback(GL30.GL_TRANSFORM_FEEDBACK_BUFFER, GL30.GL_TRANSFORM_FEEDBACK_BUFFER_BINDING),
 
     /**
      * An indexed buffer binding for buffers used as storage for uniform blocks.
@@ -111,6 +104,13 @@ public enum GLBufferTarget implements GLMagicInteger {
      */
     ShaderStorage(GL43.GL_SHADER_STORAGE_BUFFER, GL43.GL_SHADER_STORAGE_BUFFER_BINDING),
 
+    /**
+     * This is for performing direct writes from asynchronous queries to buffer object memory.  If a buffer is bound to
+     * Query, then all glGetQueryObject​ function calls will write the result to an offset into the bound buffer
+     * object.
+     */
+    Query(GL44.GL_QUERY_BUFFER, GL44.GL_QUERY_BUFFER_BINDING),
+
     // End of enumeration
     ;
 
@@ -130,20 +130,18 @@ public enum GLBufferTarget implements GLMagicInteger {
                 return GLBufferTarget.Array;
             case GL15.GL_ELEMENT_ARRAY_BUFFER:
                 return GLBufferTarget.ElementArray;
-            case GL31.GL_COPY_READ_BUFFER:
-                return GLBufferTarget.CopyRead;
-            case GL31.GL_COPY_WRITE_BUFFER:
-                return GLBufferTarget.CopyWrite;
             case GL21.GL_PIXEL_PACK_BUFFER:
                 return GLBufferTarget.PixelPack;
             case GL21.GL_PIXEL_UNPACK_BUFFER:
                 return GLBufferTarget.PixelUnpack;
-            case GL44.GL_QUERY_BUFFER:
-                return GLBufferTarget.Query;
-            case GL31.GL_TEXTURE_BUFFER:
-                return GLBufferTarget.Texture;
             case GL30.GL_TRANSFORM_FEEDBACK_BUFFER:
                 return GLBufferTarget.TransformFeedback;
+            case GL31.GL_COPY_READ_BUFFER:
+                return GLBufferTarget.CopyRead;
+            case GL31.GL_COPY_WRITE_BUFFER:
+                return GLBufferTarget.CopyWrite;
+            case GL31.GL_TEXTURE_BUFFER:
+                return GLBufferTarget.Texture;
             case GL31.GL_UNIFORM_BUFFER:
                 return GLBufferTarget.Uniform;
             case GL40.GL_DRAW_INDIRECT_BUFFER:
@@ -154,6 +152,8 @@ public enum GLBufferTarget implements GLMagicInteger {
                 return GLBufferTarget.DispatchIndirect;
             case GL43.GL_SHADER_STORAGE_BUFFER:
                 return GLBufferTarget.ShaderStorage;
+            case GL44.GL_QUERY_BUFFER:
+                return GLBufferTarget.Query;
             default:
                 throw new GLMagicIntegerException("Could not match to a GLBufferTarget.");
         }
@@ -175,20 +175,18 @@ public enum GLBufferTarget implements GLMagicInteger {
                 return GLBufferTarget.Array;
             case GL15.GL_ELEMENT_ARRAY_BUFFER_BINDING:
                 return GLBufferTarget.ElementArray;
-            case GL31.GL_COPY_READ_BUFFER_BINDING:
-                return GLBufferTarget.CopyRead;
-            case GL31.GL_COPY_WRITE_BUFFER_BINDING:
-                return GLBufferTarget.CopyWrite;
             case GL21.GL_PIXEL_PACK_BUFFER_BINDING:
                 return GLBufferTarget.PixelPack;
             case GL21.GL_PIXEL_UNPACK_BUFFER_BINDING:
                 return GLBufferTarget.PixelUnpack;
-            case GL44.GL_QUERY_BUFFER_BINDING:
-                return GLBufferTarget.Query;
-            case GL31.GL_TEXTURE_BINDING_BUFFER:
-                return GLBufferTarget.Texture;
             case GL30.GL_TRANSFORM_FEEDBACK_BUFFER_BINDING:
                 return GLBufferTarget.TransformFeedback;
+            case GL31.GL_COPY_READ_BUFFER_BINDING:
+                return GLBufferTarget.CopyRead;
+            case GL31.GL_COPY_WRITE_BUFFER_BINDING:
+                return GLBufferTarget.CopyWrite;
+            case GL31.GL_TEXTURE_BINDING_BUFFER:
+                return GLBufferTarget.Texture;
             case GL31.GL_UNIFORM_BUFFER_BINDING:
                 return GLBufferTarget.Uniform;
             case GL40.GL_DRAW_INDIRECT_BUFFER_BINDING:
@@ -199,6 +197,8 @@ public enum GLBufferTarget implements GLMagicInteger {
                 return GLBufferTarget.DispatchIndirect;
             case GL43.GL_SHADER_STORAGE_BUFFER_BINDING:
                 return GLBufferTarget.ShaderStorage;
+            case GL44.GL_QUERY_BUFFER_BINDING:
+                return GLBufferTarget.Query;
             default:
                 throw new GLMagicIntegerException("Could not match to a GLBufferTarget.");
         }
