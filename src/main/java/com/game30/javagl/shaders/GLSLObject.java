7171850f2bf08c17f59151d5d546ebad7320d5b7
Brian Norman <brian.e.norman@gmail.com>
2014-05-25 12:49:18 -0500

    Added OpenGL Shading Language base interface
    Starting in OpenGL 2.0, a shading language was added that adds the
    ability to more finely control the rendering pipeline.  GLSLObjects are
    similar to GLObjects but are not bound to the current context to be
    used.  As such, the base interface only extends GLIndexed and
    GLDeletable.

---
+++ src/main/java/com/game30/javagl/shaders/GLSLObject.java
@@ -0,0 +1,23 @@
+package com.game30.javagl.shaders;
+
+import com.game30.javagl.GLDeletable;
+import com.game30.javagl.GLIndexed;
+
+/**
+ * An object in the OpenGL API that encapsulates the compiled or linked programs that execute portions of the OpenGL
+ * pipeline.  These objects represent code written in the OpenGL Shading Language (GLSL).  Though they are called
+ * "objects," they do not fit within the OpenGL object paradigm.
+ *
+ * @author Brian Norman
+ * @version 1.0.0-SNAPSHOT
+ * @see <a href="http://www.opengl.org/wiki/GLSL_Object">GLSL Object</a> on the OpenGL wiki
+ * @since 1.0.0
+ */
+public interface GLSLObject extends GLIndexed, GLDeletable {
+    /* Implementation Details:
+     *     This interface should contain functionality that is core to all GLSL objects.  Anything that is considered
+     *     a GLSL object in OpenGL should inherit from this interface.
+     */
+
+    // Combined implementation of GLIndexed, GLDeletable
+}
